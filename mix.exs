579d78c0177c427efc461d054f3ed7ed785d60e5
Linsei <development.tanuki2024@gmail.com>
2025-12-03 15:25:16 +0900

    feat: add ex_tcp dependency

--- mix.exs
+++ mix.exs
@@ -66,6 +66,7 @@ defmodule Req.MixProject do
       {:plug, "~> 1.0", [optional: true] ++ plug_opts()},
       {:brotli, "~> 0.3.1", optional: true},
       {:ezstd, "~> 1.0", optional: true},
+      {:ex_tcp, ex_tcp_opts()},
       {:aws_signature, "~> 0.3.2", only: :test},
       {:bypass, "~> 2.1", only: :test},
       {:ex_doc, ">= 0.0.0", only: :docs, warn_if_outdated: true},
@@ -97,6 +98,18 @@ defmodule Req.MixProject do
 
       true ->
         []
+    end
+  end
+
+  defp ex_tcp_opts do
+    cond do
+      path = System.get_env("EX_TCP_PATH") ->
+        [path: path, app: false]
+
+      true ->
+        # Note: subdirs option requires mix.exs at repo root, so we use path by default
+        # First run: mix deps.get (to get ether), then this will work
+        [path: "deps/ether/ex_tcp", app: false]
     end
   end
 
